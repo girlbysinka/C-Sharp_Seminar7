80f04603d434dae61913dbb35efbb9e4ee7ba34f
«Evgeniia» <girlbysinka@mail.ru>
2022-08-30 23:34:29 +0300

    add hw_task47

---
+++ hw_task47/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Clear();
+
+double [,] matrix = new double [3,4];
+
+GetRandomMatrix (matrix);
+
+
+void GetRandomMatrix (double[,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                    {
+                    matr[i, j] = new Random().Next(-10,10);
+                    Console.Write($"{matr[i, j]} ");
+                    }
+                Console.WriteLine();
+            }
+    } 
+
